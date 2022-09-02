adec17f26113e45232bf20993738a86910471aec
Andrew Nguyen <andrew.nguyen.18@cnu.edu>
2022-09-02 07:50:15 -0400

    Initial Commit

---
+++ Project 1 Slots/Project_1_SlotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_1_SlotsApp.swift
+//  Project 1 Slots
+//
+//  Created by Andrew Nguyen on 9/2/22.
+//
+
+import SwiftUI
+
+@main
+struct Project_1_SlotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
